0a81a355324a546fdc6f5bd2a32e3d8ad1909fa1
Kushagra Upadhyaya <kushagra23dec@gmail.com>
2023-02-06 00:30:41 +0530

    cc

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,13 +9,13 @@ import useRestaurants from "../utils/useRestaurants";
 const Body = () => {
     const restaurants = useRestaurants();
 
-    return (restaurants.length === 0) ?
+    return (restaurants?.length === 0) ?
         <Shimmer />
         : (
             <div className="flex flex-wrap grow justify-center mt-8 mx-3 " >
 
-                {restaurants.map(
-                    (restaurant) => { return <Link key={restaurant.data.id} to={"/carddetail/" + restaurant.data.id} ><Card  {...restaurant.data} /> </Link> })
+                {restaurants?.map(
+                    (restaurant) => { return <Link key={restaurant?.data?.id} to={"/carddetail/" + restaurant?.data?.id} ><Card  {...restaurant?.data} /> </Link> })
                 }
 
             </div >
