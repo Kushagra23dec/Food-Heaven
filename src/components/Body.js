b1bda6fc04653da962bee9f9b0c52f85cec6a615
Kushagra Upadhyaya <kushagra23dec@gmail.com>
2023-02-06 00:29:24 +0530

    cc

--- src/components/Body.js
+++ src/components/Body.js
@@ -14,21 +14,21 @@ const Body = () => {
         : (
             <div className="flex flex-wrap grow justify-center mt-8 mx-3 " >
 
-                {restaurants.map((restaurant) => { return <Link key={restaurant.data.id} to={"/carddetail/" + restaurant.data.id} ><Card  {...restaurant.data} /></Link> })}
+                {restaurants.map(
+                    (restaurant) => { return <Link key={restaurant.data.id} to={"/carddetail/" + restaurant.data.id} ><Card  {...restaurant.data} /> </Link> })
+                }
 
-<<<<<<< HEAD
             </div >
         )
-=======
-
-    return(restaurants.length === 0)?
-   <Shimmer />
-    :(
-        <div className="flex flex-wrap grow justify-center mt-8 mx-3 " >
 
 
-        </div >
-    )
+
+
+
+
+
+
+
 }
 
 export default Body;
