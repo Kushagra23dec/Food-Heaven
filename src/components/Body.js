77f248eb55bcde536a9629a27155dc95407b4e43
Kushagra Upadhyaya <kushagra23dec@gmail.com>
2023-02-08 19:05:06 +0530

    added Search Functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,34 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import { Link } from "react-router-dom";
 import Shimmer from "./Shimmer.js";
 import useRestaurants from "../utils/useRestaurants";
 
 
+const handleSearch = (restaurant,searchTxt,setFilteredRestaurants)=>{
+
+
+    if(searchTxt.length === 0 || searchTxt === " "){
+        setFilteredRestaurants(restaurant);
+        return;
+    }else {
+        const filteredData = restaurant.filter((restaurantName)=>{return restaurantName.data.name.toLowerCase().includes(searchTxt.toLowerCase()) })
+        console.log(filteredData);
+        setFilteredRestaurants(filteredData);
+        
+    }
+
+}
+
 
 const Body = () => {
-    const restaurants = useRestaurants();
+    const [filteredRestaurants,setFilteredRestaurants] = useState([]);
+    const restaurants = useRestaurants(setFilteredRestaurants);
+    const [searchTxt,setSearchTxt] = useState("");
 
-    return (restaurants?.length === 0) ?
-        <Shimmer />
+    return (filteredRestaurants?.length === 0) ?
+        <div>
+
+             <div className=" flex justify-center p-3 bg-gray-600 flex-wrap ">
+                <input className="p-2 text-lg w-1/5 rounded-l-md focus:bg-amber-200" type="text" 
+                value={searchTxt} placeholder="Restaurant" onChange={(e)=>{setSearchTxt(e.target.value)}} />
+                <button className="text-lg font-semibold px-1 bg-amber-400 rounded-r-md" onClick={()=>{handleSearch(restaurants,searchTxt,setFilteredRestaurants)}}>Search</button>
+                </div>
+                
+                <Shimmer />
+        </div>
         : (
+            <div className="grow">
+             
+                <div className=" flex justify-center p-3 bg-gray-600 flex-wrap ">
+                <input className="p-2 text-lg w-1/2 rounded-l-md focus:bg-amber-200" type="text" 
+                value={searchTxt} placeholder="Restaurant" onChange={(e)=>{setSearchTxt(e.target.value)}} />
+                <button className="text-lg font-semibold px-1 bg-amber-400 rounded-r-md" onClick={()=>{handleSearch(restaurants,searchTxt,setFilteredRestaurants)}}>Search</button>
+                </div>
+
+
             <div className="flex flex-wrap grow justify-center mt-8 mx-3 " >
 
-                {restaurants?.map(
-                    (restaurant) => { return <Link key={restaurant?.data?.id} to={"/carddetail/" + restaurant?.data?.id} ><Card  {...restaurant?.data} /> </Link> })
+                {filteredRestaurants?.map(
+                    (filteredRestaurant) => { return <Link key={filteredRestaurant?.data?.id} to={"/carddetail/" + filteredRestaurant?.data?.id} ><Card  {...filteredRestaurant?.data} /> </Link> })
                 }
 
             </div >
+            </div>
         )
 
-
-
-
-
-
-
-
-
+ 
 }
 
 export default Body;
--- src/utils/useRestaurants.js
+++ src/utils/useRestaurants.js
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react";
 import { RESTAURANT_URL } from "../assets/resources";
 
-async function fetchData(setRestaurants) {
+async function fetchData(setRestaurants,setFilteredRestaurants) {
     const response = await fetch(RESTAURANT_URL);
     const data = await response.json();
     // console.log(data?.data?.cards[2]?.data?.data?.cards);
     setRestaurants(data?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(data?.data?.cards[2]?.data?.data?.cards);
 
 }
 
-const useRestaurants = () => {
+const useRestaurants = (setFilteredRestaurants) => {
     const [restaurants, setRestaurants] = useState([]);
     useEffect(
         () => {
-            fetchData(setRestaurants);
+            fetchData(setRestaurants,setFilteredRestaurants);
         },
         []);
 
