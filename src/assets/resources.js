327f4712b527e16e436b963e44a46fadc26f19f3
kushagra23dec <kushagra23dec@gmail.com>
2023-02-20 23:41:17 +0530

    cc

--- src/assets/resources.js
+++ src/assets/resources.js
@@ -1,6 +1,6 @@
 export const LOGO_URL = "https://lh3.googleusercontent.com/Em7AHf7XBH_RtGfCBVXz9RH8SM_pHkj3xPP-yd3cRguY1_Jc8fmqgx6WxnvGVyPV5xs5gL3HCD0FCuv6Xo4CwoY6ak4";
 
-export const RESTAURANT_URL = "https://pastebin.com/raw/vTzuMyjm";
+export const RESTAURANT_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=29.9456906&lng=78.16424780000001&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_URL = "https://www.swiggy.com/dapi/menu/v4/full?lat=29.9456906&lng=78.16424780000001&menuId=";
 
