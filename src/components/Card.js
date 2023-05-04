e01516789f8d5da0a665272c3f1efdde9604acae
kushagra23dec <kushagra23dec@gmail.com>
2023-05-04 14:31:02 +0530

    did some minor changes.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { CLOUDINARY_IMG_BASE_URL } from "../assets/resources";
 import NO_IMAGE2 from "../assets/NO_IMAGE.png";
-
+import {BiFoodTag} from "react-icons/bi"
+import {BsFillCaretUpSquareFill} from "react-icons/bs";
 const Card = ({ cloudinaryImageId,
   name,
   cuisines,
@@ -10,6 +11,7 @@ const Card = ({ cloudinaryImageId,
   area,
   minDeliveryTime,
   maxDeliveryTime,
+  veg,
   avgRating, }) => {
   return (
     <div className="shadow-2xl p-2   bg-amber-50 my-2 mx-2 w-64  rounded-md">
@@ -17,8 +19,8 @@ const Card = ({ cloudinaryImageId,
       <h1 className="text-lg font-bold">{name}</h1>
       <h1 className="font-semibold text-gray-800">{cuisines[0]} {cuisines.length >= 2 ? "," : ""} {cuisines.length >= 2 && cuisines[1]}  </h1>
       <div className=" px-2 flex justify-between ">
-
         <span className=" text-sm   font-semibold text-gray-800">{avgRating === "--" ? null : avgRating}</span>
+        <span>{(veg) ? <BiFoodTag title="VEG" className="text-green-600 text-xl"/> : <BsFillCaretUpSquareFill title="NON-VEG" className="text-red-600 text-xl"/>}</span>
         <h1 className="text-sm font-semibold text-gray-800">{maxDeliveryTime} MINS</h1>
       </div>
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
                 <h1 className="text-gray-900 text-xl ml-4">This site is developed By: Kushagra Upadhyaya.</h1>
             </div>
             <div className="text-xl font-semibold flex gap-4 mr-5">
+                <a href="https://github.com/Kushagra23dec/Food-Heaven" target="_blank"><h1 className="">Source Code</h1></a>
                 <a href="https://www.github.com/kushagra23dec" target="_blank"><h1 className="">Github</h1></a>
                 <a href="https://leetcode.com/Kushagra23dec/" target="_blank"><h1 className="">Leetcode</h1></a>
             </div>
